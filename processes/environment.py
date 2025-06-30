36cfdfd1963823af8f31400c292ca97e5fcc908d
VirtualLab <virtual.lab.2000@gmail.com>
2025-06-30 23:17:30 +0200

    Energy and molecules transfer

--- processes/environment.py
+++ processes/environment.py
@@ -18,4 +18,46 @@ def update_environment(voxels, property_map, dt=1.0):
     changed = apply_gravity(voxels, dt=dt)
     for prop in ['humidity', 'heat', 'water', 'nutrient']:
         transfer_property(voxels, property_map, prop, dt=dt)
+    # --- Water block height and spill logic ---
+    from world.voxel import Voxel
+    to_remove = set()
+    to_add = []
+    for (x, y, z) in list(voxels):
+        props = property_map.get((x, y, z), {})
+        if props.get('type') == 'water':
+            water = props.get('water', 0)
+            # Set block height proportional to water content (max 1.0)
+            h = max(0.05, min(1.0, water))
+            # Update block_height if present
+            v = None
+            for vv in voxels:
+                if vv.x == x and vv.y == y and vv.z == z:
+                    v = vv
+                    break
+            if v is not None:
+                v.block_height = h
+            # Remove water block if almost empty
+            if water < 0.01:
+                to_remove.add(v)
+            # Spill to lower neighbor if possible
+            below = Voxel(x, y-1, z)
+            if below in voxels:
+                bprops = property_map.get((below.x, below.y, below.z), {})
+                if bprops.get('type') == 'water' and bprops.get('water', 0) < water:
+                    # Transfer some water down
+                    transfer = min(0.1, water - bprops.get('water', 0))
+                    props['water'] -= transfer
+                    bprops['water'] += transfer
+            elif y > 0:
+                # Create new water block below if not solid
+                bprops = property_map.get((x, y-1, z), {})
+                if bprops.get('type', 'air') == 'air':
+                    if water > 0.1:
+                        to_add.append((Voxel(x, y-1, z), {'type': 'water', 'water': water*0.5, 'humidity': 1.0, 'heat': 0.5, 'nutrient': 0.5}))
+                        props['water'] *= 0.5
+    for v in to_remove:
+        voxels.remove(v)
+    for v, p in to_add:
+        voxels.add(v)
+        property_map[(v.x, v.y, v.z)] = p
     return changed
--- processes/transfer.py
+++ processes/transfer.py
@@ -3,6 +3,34 @@ Transfer process module for the voxel world simulation.
 Handles humidity, heat, water, and nutrient transfer between adjacent blocks.
 """
 
+def get_transfer_coeff(prop, props):
+    """
+    Return a transfer coefficient for the property based on block composition.
+    - For heat: higher for water and minerals (esp. quartz), lower for organic.
+    - For humidity: higher for water, clay, organic.
+    - For nutrient: higher for water, organic, clay.
+    """
+    minerals = props.get('minerals_comp', {})
+    inorg = props.get('inorganic_comp', {})
+    organic = props.get('organic_comp', {})
+    # Example: use quartz, clay, water, organic fractions
+    quartz = minerals.get('quartz', 0)
+    clay = minerals.get('clay', 0)
+    water = inorg.get('water', 0)
+    humus = organic.get('humus', 0)
+    if prop == 'heat':
+        # Thermal conductivity: quartz > water > clay > organic
+        return 0.2 + 0.6*quartz + 0.4*water + 0.2*clay + 0.05*humus
+    elif prop == 'humidity':
+        # Water diffusivity: water > clay > organic > quartz
+        return 0.1 + 0.5*water + 0.3*clay + 0.2*humus + 0.05*quartz
+    elif prop == 'nutrient':
+        # Nutrient mobility: water > organic > clay > quartz
+        return 0.1 + 0.5*water + 0.3*humus + 0.2*clay + 0.05*quartz
+    else:
+        return 0.1
+
+
 def transfer_property(voxels, property_map, prop, rate=0.1, dt=1.0):
     """
     Simulate transfer of a property (e.g., humidity, heat, water, nutrient) between adjacent voxels.
@@ -16,14 +44,19 @@ def transfer_property(voxels, property_map, prop, rate=0.1, dt=1.0):
     from collections import defaultdict
     from world.voxel import Voxel
     deltas = defaultdict(float)
-    eff_rate = rate * dt
     for (x, y, z) in voxels:
-        val = property_map.get((x, y, z), {}).get(prop, 0)
+        props = property_map.get((x, y, z), {})
+        val = props.get(prop, 0)
+        coeff = get_transfer_coeff(prop, props) * rate * dt
         for dx, dy, dz in [(-1,0,0),(1,0,0),(0,-1,0),(0,1,0),(0,0,-1),(0,0,1)]:
             n = Voxel(x+dx, y+dy, z+dz)
             if n in voxels:
-                nval = property_map.get((n.x, n.y, n.z), {}).get(prop, 0)
-                diff = (val - nval) * eff_rate
+                nprops = property_map.get((n.x, n.y, n.z), {})
+                nval = nprops.get(prop, 0)
+                ncoeff = get_transfer_coeff(prop, nprops) * rate * dt
+                # Use harmonic mean for interface
+                eff_coeff = 2 * coeff * ncoeff / (coeff + ncoeff) if (coeff + ncoeff) > 0 else 0
+                diff = (val - nval) * eff_coeff
                 deltas[(x, y, z)] -= diff
                 deltas[(n.x, n.y, n.z)] += diff
     for k, d in deltas.items():
